f720a3c533a365b42dc430348875734faa6d56de
chuanbao lu <chuanbao.lu@gmail.com>
2021-12-14 12:19:41 -0500

    clean up code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
     <i18n path=""></i18n>
-    <BaseNotice />
     <router-view />
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
-import BaseNotice from "@/components/BaseNotice.vue";
 import { ActionTypes } from "./store/actions";
 export default Vue.extend({
   name: "App",
-  components: {
-    BaseNotice
-  },
+  components: {},
   created() {
     if (this.$store.getters.isInitialized === false) {
       this.$store.dispatch(ActionTypes.SetAppData);
