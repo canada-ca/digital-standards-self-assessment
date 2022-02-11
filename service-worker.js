31ef640b84785aaa358b18e5d1d5c3f5bc858bc6
lcbdl <lcbdl@users.noreply.github.com>
2022-02-11 02:26:56 +0000

    deploy: 554cce91bf879019d0f2c1f8bf10dc97dcb9a5af

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/digital-standards-self-assessment/precache-manifest.08ead18229a2570ad521afa36c33be10.js"
+  "/digital-standards-self-assessment/precache-manifest.05eecf316893085983c9a40bd12d8335.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "digital-standards-self-assessment"});
