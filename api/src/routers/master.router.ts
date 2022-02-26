341e04c4e2d0b582a53576fbfb926d7643413f41
chuanbao lu <chuanbao.lu@gmail.com>
2022-02-25 23:29:03 -0500

    add swagger-ui for api doc

--- api/src/routers/master.router.ts
+++ api/src/routers/master.router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import userRouter from './user.router';
 import pingController from '../controllers/ping.controller';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDoc from '../swagger.json';
 
 class MasterRouter {
   private _router = Router();
@@ -18,6 +20,7 @@ class MasterRouter {
    */
   private _configure() {
     this._router.get('/ping', pingController.ping);
+    this._router.use('/api-docs', serve, setup(swaggerDoc));
     this._router.use('/user', userRouter);
     // Add other routers below
   }
