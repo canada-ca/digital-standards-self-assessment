90556dc22f12061228b6e08bcaea7af4921a60df
chuanbao lu <chuanbao.lu@gmail.com>
2022-03-03 22:51:39 -0500

    prepare to add azure function API

--- api/src/routers/master.router.ts
+++ api/src/routers/master.router.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import userRouter from './user.router';
 import pingController from '../controllers/ping.controller';
-import { serve, setup } from 'swagger-ui-express';
-import swaggerDoc from '../swagger.json';
 
 class MasterRouter {
   private _router = Router();
@@ -20,7 +18,6 @@ class MasterRouter {
    */
   private _configure() {
     this._router.get('/ping', pingController.ping);
-    this._router.use('/api-docs', serve, setup(swaggerDoc));
     this._router.use('/user', userRouter);
     // Add other routers below
   }
