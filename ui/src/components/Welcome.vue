791bd4a8a60c9d70ee001555aae601cc9d0cc837
chuanbao lu <chuanbao.lu@gmail.com>
2022-03-24 21:32:01 -0400

    main page enhancement

--- ui/src/components/Welcome.vue
+++ ui/src/components/Welcome.vue
@@ -2,75 +2,54 @@
   <div class="mb-3">
     <section style="background: #e0edff">
       <div class="container">
-        <h1>{{ $t("notice.startUsingTitle") }}</h1>
+        <h1>{{ $t('notice.startUsingTitle') }}</h1>
         <div v-html="markdownToHtml($t('notice.startUsingDesc'))" />
         <div class="col-container">
           <div class="welcome-col left-col">
-            <b-img
-              :src="require('@/assets/images/items.png')"
-              style="height: 35px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/items.png')" style="height: 35px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.takeTheSurvey") }}
+                {{ $t('notice.takeTheSurvey') }}
               </p>
               <p v-html="markdownToHtml($t('notice.takeTheSurveyDesc'))"></p>
             </span>
           </div>
           <div class="welcome-col right-col">
-            <b-img
-              :src="require('@/assets/images/bars.png')"
-              style="height: 42px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/bars.png')" style="height: 42px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.calculateTeamResults") }}
+                {{ $t('notice.calculateTeamResults') }}
               </p>
-              <p
-                v-html="markdownToHtml($t('notice.calculateTeamResultsDesc'))"
-              />
+              <p v-html="markdownToHtml($t('notice.calculateTeamResultsDesc'))" />
             </span>
           </div>
         </div>
       </div>
     </section>
     <section class="container">
-      <h2 id="welcomeNoticeTitle">{{ $t("notice.welcomeNoticeTitle") }}</h2>
-      <div
-        style="margin: 50px 0px"
-        v-html="markdownToHtml($t('notice.welcomeNoticeBody'))"
-      ></div>
+      <h2 style="margin-top: 30px" id="welcomeNoticeTitle">{{ $t('notice.welcomeNoticeTitle') }}</h2>
+      <div style="margin: 30px 0px" v-html="markdownToHtml($t('notice.welcomeNoticeBody'))"></div>
     </section>
     <section style="background: #e0edff">
       <div class="container">
-        <h2>
-          {{ $t("notice.subtitle1") }}
+        <h2 style="margin-top: 30px">
+          {{ $t('notice.subtitle1') }}
         </h2>
         <div class="col-container">
           <div class="welcome-col left-col">
-            <b-img
-              :src="require('@/assets/images/steps.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/steps.png')" style="height: 60px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.section1p1") }}
+                {{ $t('notice.section1p1') }}
               </p>
               <p v-html="markdownToHtml($t('notice.section1p2'))"></p>
             </span>
           </div>
           <div class="welcome-col right-col">
-            <b-img
-              :src="require('@/assets/images/step.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/step.png')" style="height: 50px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.section1p3") }}
+                {{ $t('notice.section1p3') }}
               </p>
               <p v-html="markdownToHtml($t('notice.section1p4'))" />
             </span>
@@ -80,32 +59,24 @@
     </section>
     <section>
       <div class="container">
-        <h2>
-          {{ $t("notice.subtitle2") }}
+        <h2 style="margin-top: 30px">
+          {{ $t('notice.subtitle2') }}
         </h2>
         <div v-html="markdownToHtml($t('notice.section2p1'))" />
         <div class="col-container">
           <div class="welcome-col left-col">
-            <b-img
-              :src="require('@/assets/images/time2.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/time2.png')" style="height: 60px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.section2p2") }}
+                {{ $t('notice.section2p2') }}
               </p>
             </span>
           </div>
           <div class="welcome-col right-col">
-            <b-img
-              :src="require('@/assets/images/time1.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+            <b-img :src="require('@/assets/images/time1.png')" style="height: 60px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.section2p3") }}
+                {{ $t('notice.section2p3') }}
               </p>
             </span>
           </div>
@@ -114,34 +85,17 @@
     </section>
     <section style="background: #e0edff">
       <div class="container">
-        <h2>
-          {{ $t("notice.subtitle3") }}
+        <h2 style="margin-top: 30px">
+          {{ $t('notice.subtitle3') }}
         </h2>
         <div class="col-container">
-          <div class="welcome-col left-col">
-            <b-img
-              :src="require('@/assets/images/brush.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
+          <div class="welcome-col" style="flex-basis: 100%">
+            <b-img :src="require('@/assets/images/brush.png')" style="height: 45px; margin: 8px 20px 0px 0px" alt="" />
             <span>
               <p style="font-size: 1.4em">
-                {{ $t("notice.section3p1") }}
+                {{ $t('notice.section3p1') }}
               </p>
               <p v-html="markdownToHtml($t('notice.section3p2'))"></p>
-            </span>
-          </div>
-          <div class="welcome-col right-col">
-            <b-img
-              :src="require('@/assets/images/exclamation.png')"
-              style="height: 60px; margin: 8px 20px 0px 0px"
-              alt=""
-            />
-            <span>
-              <p style="font-size: 1.4em">
-                {{ $t("notice.section3p3") }}
-              </p>
-              <p v-html="markdownToHtml($t('notice.section3p4'))" />
             </span>
           </div>
         </div>
@@ -151,11 +105,11 @@
 </template>
 
 <script lang="ts">
-import { marked } from "marked";
-import { Component, Vue } from "vue-property-decorator";
-import ShowHideLink from "./ShowHideLink.vue";
+import { marked } from 'marked';
+import { Component, Vue } from 'vue-property-decorator';
+import ShowHideLink from './ShowHideLink.vue';
 @Component({
-  components: { ShowHideLink }
+  components: { ShowHideLink },
 })
 export default class Welcome extends Vue {
   markdownToHtml(item: string) {
@@ -171,7 +125,7 @@ export default class Welcome extends Vue {
 }
 .col-container {
   display: flex;
-  margin: 50px 0px;
+  margin: 30px 0px;
 }
 .left-col {
   padding-right: 30px;
@@ -180,7 +134,7 @@ export default class Welcome extends Vue {
   padding-left: 30px;
 }
 h2 {
-  font-size: 48px !important;
+  font-size: 36px !important;
   font-weight: 300 !important;
 }
 </style>
