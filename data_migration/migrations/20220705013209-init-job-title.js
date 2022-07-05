4a4656a22fe18577bd280526f9ba9d9f6b12b27e
chuanbao lu <chuanbao.lu@gmail.com>
2022-07-04 22:29:27 -0400

    jobTitle migration script.

--- data_migration/migrations/20220705013209-init-job-title.js
+++ data_migration/migrations/20220705013209-init-job-title.js
@@ -1,10 +1,10 @@
 const jobTitles = require('./20220705013209-Job-titles.json');
 module.exports = {
   async up(db, client) {
-    await db.collection('jobTitles').insertMany(jobTitles);
+    await db.collection('jobTitle').insertMany(jobTitles);
   },
 
   async down(db, client) {
-    await db.collection('jobTitles').drop();
+    await db.collection('jobTitle').drop();
   },
 };
